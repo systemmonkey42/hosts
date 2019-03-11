4667fbb5112a68af20cfe1aaf5d4d3beed984b74
David le Blanc <david-le-blanc@users.noreply.github.com>
2019-03-11 15:51:02 +1100

    Updated hosts - added script to run from cron and update dnsmasq

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+exec > /tmp/update.log 2>&1
+
+set -x
+
+BASE="${0%/*}"
+
+cd "${BASE}" && python3 updateHostsFile.py --output /etc/dnshosts/  --auto --skipstatichosts --extensions porn
+reload dnsmasq
